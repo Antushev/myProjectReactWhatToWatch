4725ca476e8fb08f7364dd0dc1c15ffca06ed93f
DimAntushev <fyneitd@mail.ru>
2020-07-25 18:23:41 +0300

    Исправляет отступы

--- src/components/film-detail-more/film-detail-more.jsx
+++ src/components/film-detail-more/film-detail-more.jsx
@@ -17,47 +17,47 @@ const FilmDetailMore = (props) => {
 
   return (
     <div className="movie-card__wrap movie-card__translate-top">
-        <div className="movie-card__info">
-          <div className="movie-card__poster movie-card__poster--big">
-            <img src={`img/${posterImage}`} alt={name} width="218"
-              height="327"/>
-          </div>
+      <div className="movie-card__info">
+        <div className="movie-card__poster movie-card__poster--big">
+          <img src={`img/${posterImage}`} alt={name} width="218"
+            height="327"/>
+        </div>
 
-          <div className="movie-card__desc">
-            {renderTabs()}
+        <div className="movie-card__desc">
+          {renderTabs()}
 
-            <div className="movie-card__text movie-card__row">
-              <div className="movie-card__text-col">
-                <p className="movie-card__details-item">
-                  <strong className="movie-card__details-name">Director</strong>
-                  <span className="movie-card__details-value">{director}</span>
-                </p>
-                <p className="movie-card__details-item">
-                  <strong className="movie-card__details-name">Starring</strong>
-                  <span className="movie-card__details-value">
-                    {starring}
-                  </span>
-                </p>
-              </div>
+          <div className="movie-card__text movie-card__row">
+            <div className="movie-card__text-col">
+              <p className="movie-card__details-item">
+                <strong className="movie-card__details-name">Director</strong>
+                <span className="movie-card__details-value">{director}</span>
+              </p>
+              <p className="movie-card__details-item">
+                <strong className="movie-card__details-name">Starring</strong>
+                <span className="movie-card__details-value">
+                  {starring}
+                </span>
+              </p>
+            </div>
 
-              <div className="movie-card__text-col">
-                <p className="movie-card__details-item">
-                  <strong className="movie-card__details-name">Run Time</strong>
-                  <span className="movie-card__details-value">{runtime}</span>
-                </p>
-                <p className="movie-card__details-item">
-                  <strong className="movie-card__details-name">Genre</strong>
-                  <span className="movie-card__details-value">{genre}</span>
-                </p>
-                <p className="movie-card__details-item">
-                  <strong className="movie-card__details-name">Released</strong>
-                  <span className="movie-card__details-value">{date}</span>
-                </p>
-              </div>
+            <div className="movie-card__text-col">
+              <p className="movie-card__details-item">
+                <strong className="movie-card__details-name">Run Time</strong>
+                <span className="movie-card__details-value">{runtime}</span>
+              </p>
+              <p className="movie-card__details-item">
+                <strong className="movie-card__details-name">Genre</strong>
+                <span className="movie-card__details-value">{genre}</span>
+              </p>
+              <p className="movie-card__details-item">
+                <strong className="movie-card__details-name">Released</strong>
+                <span className="movie-card__details-value">{date}</span>
+              </p>
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
